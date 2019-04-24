7469d86d5eeabc0d88d20a7055d281e571619ad1
Long Nguyen <lpn@mit.edu>
2019-04-24 12:43:29 -0400

    Added basic files for implementation demo; need to continue

---
+++ user-training.py
@@ -0,0 +1,11 @@
+from GRTPy3 import GRT
+import sys
+import argparse
+
+'''
+Implementation plan:
+CLI for user to do a gesture and save it to some command.
+Script saves training data to a file and trains GRT in the background.
+Deletes file when complete, but maybe users should be able to see what they have stored.
+Add parsing options to see what you have stored.
+'''
