9972d0fa129d647ce9591c172d65712f8274735d
Long Nguyen <lpn@mit.edu>
2019-04-25 05:41:54 -0400

    Added basic ML skeleton for GRT, not feasible right now; supported keyboard inputs

--- implementation-demo.py
+++ implementation-demo.py
@@ -1,12 +1,117 @@
+import inspect
+import os
+import sys
+import time
+
+from GRTPy3 import GRT
+from LeapSDKPy3 import Leap
+from LeapSDKPy3.Leap import CircleGesture
+from LeapSDKPy3.Leap import KeyTapGesture
+from LeapSDKPy3.Leap import ScreenTapGesture
+from LeapSDKPy3.Leap import SwipeGesture
 from imports import *
-from GRTPy3.GRT import *
+from pykeyboard import PyKeyboard
+from pymouse import PyMouse
+from pynput import keyboard as Keyboard
+from pynput.keyboard import Key
 
-pipeline = GestureRecognitionPipeline()
-pipeline.addPreProcessingModule(MovingAverageFilter(5, 1))
-pipeline.addFeatureExtractionModule( FFT(512) );
 
-# pipeline.addFeatureExtractionModule( MyOwnFeatureMethod() );
+def new_iterm_pane():
+    global keyboard
+    keyboard.press(Key.cmd)
+    keyboard.press(Key.shift)
+    keyboard.press('d')
+    keyboard.release('d')
+    keyboard.release(Key.shift)
+    keyboard.release(Key.cmd)
 
-pipeline.setClassifier( ANBC() );
 
-pipeline.addPostProcessingModule( ClassLabelTimeoutFilter(1000) );
+def next_iterm_pane():
+    global keyboard
+    keyboard.press(Key.cmd)
+    keyboard.press(']')
+    keyboard.release(']')
+    keyboard.release(Key.cmd)
+    time.sleep(0.25)
+
+
+def prev_iterm_pane():
+    global keyboard
+    keyboard.press(Key.cmd)
+    keyboard.press('[')
+    keyboard.release('[')
+    keyboard.release(Key.cmd)
+    time.sleep(0.25)
+
+
+class GesTermListener(Leap.Listener):
+
+    def on_exit(self, controller):
+        print('Exited Leap Motion.')
+
+    def on_disconnect(self, controller):
+        print('Disconnected from Leap Motion controller.')
+
+
+class CircleListener(GesTermListener):
+
+    def __print_frame_info__(self, f):
+        print("Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (
+            f.id, f.timestamp, len(f.hands), len(f.fingers)))
+
+    def on_connect(self, controller):
+        print("Connected to the Leap Motion sensor.")
+
+    def on_frame(self, controller):
+        global keyboard
+        global CURRENT_CIRCLE_ID
+        frame = controller.frame()
+        prev_frame = controller.frame(1)
+        gestures = frame.gestures()
+        for gesture in gestures:
+            if gesture.type == Leap.Gesture.TYPE_CIRCLE:
+                circle = Leap.CircleGesture(gesture)
+                state = circle.state
+                if state == Leap.Gesture.STATE_START:
+                    CURRENT_CIRCLE_ID = circle.id
+                    print('Started circle gesture.')
+                if state == Leap.Gesture.STATE_STOP:
+                    print('Circle gesture completed.')
+                    time.sleep(0.3)
+                    k.type_string('ssh lpn@uat.mit.edu')
+                    keyboard.press(Key.enter)
+                    keyboard.release(Key.enter)
+
+
+def main():
+
+    # Set up the Leap Motion sensor.
+    listener = CircleListener()
+    controller = Leap.Controller()
+    controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE)
+
+    '''
+    UNCOMMENT TO SET CIRCLE GESTURE PARAMETERS.
+    # controller.config.set("Gesture.Circle.MinRadius", 10.0)
+    # controller.config.set("Gesture.Circle.MinArc", .5)
+    '''
+
+    controller.config.save()
+    # Have the listener receive events from the controller.
+    controller.add_listener(listener)
+    controller.set_policy(Leap.Controller.POLICY_BACKGROUND_FRAMES)
+    controller.set_policy(Leap.Controller.POLICY_IMAGES)
+    controller.config.save()
+
+    # Set up the pynput controller.
+    global keyboard
+    keyboard = Keyboard.Controller()
+    global k
+    k = PyKeyboard()
+
+    while True:
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
--- user-training.py
+++ user-training.py
@@ -1,6 +1,10 @@
 from GRTPy3 import GRT
-import sys
+from LeapSDKPy3 import Leap
+
 import argparse
+import ctypes
+import numpy as np
+import sys
 
 '''
 Implementation plan:
@@ -8,4 +12,100 @@ CLI for user to do a gesture and save it to some command.
 Script saves training data to a file and trains GRT in the background.
 Deletes file when complete, but maybe users should be able to see what they have stored.
 Add parsing options to see what you have stored.
+
+for tracking user movement:
+only start recording frames once 1 hand is detected
+stop and save as training data when no hands detected (finished)
 '''
+class GestureListener(Leap.Listener):
+
+    def __get_image_info__(self, i):
+        return i.width, i.height, i.bytes_per_pixel
+
+    def __get_num_points__(self, i):
+        w, h, b = self.__get_image_info__(i)
+        return w * h * b
+
+    def __print_frame_info__(self, f):
+        print("Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (
+            f.id, f.timestamp, len(f.hands), len(f.fingers)))
+
+    def on_connect(self, controller):
+        # CONNECTED = True
+        print("Connected to the Leap Motion sensor.")
+        self.gesture_in_progress = False
+        self.frames = []
+        self.cur_gest = []
+
+    def begin_macro(self):
+        self.gesture_in_progress = True
+
+    def on_frame(self, controller):
+ 
+        frame = controller.frame()
+
+        if frame.hands.is_empty and self.gesture_in_progress:
+            self.gesture_in_progress = False
+            self.frames.append(cur_gest)
+        # Only work with data if we detect a hand in the frame.
+        if len(frame.hands) != 1:
+            return
+        if not self.gesture_in_progress:
+            self.begin_macro()
+
+        images = frame.images
+        # For now, let's just work with one image from stereo.
+        image = images[1]
+        image_data = image.data
+        num_points = self.__get_num_points__(image)
+        frame_data = []
+        for i in range(num_points):
+            frame_data.append(image_data[i])
+        frame_data = np.array(frame_data)
+        if self.gesture_in_progress:
+            self.cur_gest.append(frame_data)
+
+
+'''
+PARSING SECTION
+parser = argparse.ArgumentParser(description='.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='interger list')
+parser.add_argument('--sum', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+args = parser.parse_args()
+'''
+
+def main():
+
+    # Set up the Leap Motion sensor.
+    listener = GestureListener()
+    controller = Leap.Controller()
+    # Have the listener receive events from the controller.
+    controller.add_listener(listener)
+    # MUST allow frames and images or no data will be received.
+    # https://developer-archive.leapmotion.com/documentation/python/api/Leap.Controller.html#Leap.Controller.set_policy
+    controller.set_policy(Leap.Controller.POLICY_BACKGROUND_FRAMES)
+    controller.set_policy(Leap.Controller.POLICY_IMAGES)
+    controller.config.save()
+
+    commands = {}
+    while True:
+        train = input('Would you like to train a new gestural macro?')
+        if 'y' in train or 'yes' in train or 'Yes' in train or 'Y' in train:
+            cmd = input('Please enter the command you would like to train:\n > ')
+            commands[cmd] = [] # Set up the data frames for the command.
+            print('We will have you train the system three times.')
+
+        else:
+            break
+    # Remove the sample listener when done.
+    controller.remove_listener(listener)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
